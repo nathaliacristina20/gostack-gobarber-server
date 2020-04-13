62583acfa1141a630b20999a2dfe2c26c2104356
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-13 19:21:42 -0300

    ESLint

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
-    console.log('Server started on port 33333');
+  console.log('Server started on port 33333');
 });
