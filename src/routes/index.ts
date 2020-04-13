d4307a5afb9adeb9f978d95112ff4d18c69ba123
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-13 19:57:37 -0300

    Debugando NodeJS

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,13 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-    response.json({ message: 'Hello World' }),
-);
+routes.post('/users', (request, response) => {
+    const { name, email } = request.body;
+    const user = {
+        name,
+        email,
+    };
+    response.json(user);
+});
 
 export default routes;
