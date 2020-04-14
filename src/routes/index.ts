677ea044835c93ab71696a7a5861617b0ec48d76
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-14 19:10:16 -0300

    Cadastro de Agendamentos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-    const { name, email } = request.body;
-    const user = {
-        name,
-        email,
-    };
-    response.json(user);
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
