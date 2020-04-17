94ca72c00198c26a4c1fc989481daf0d7050868e
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-16 21:21:46 -0300

    Repositorio do TypeORM

--- src/database/migrations/1587079393904-CreateAppointments.ts
+++ src/database/migrations/1587079393904-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587079393904
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
--- src/repositories/AppointmentRepository.ts
+++ src/repositories/AppointmentRepository.ts
@@ -1,32 +1,15 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-    provider: string;
-    date: Date;
-}
-class AppointmentsRepository {
-    private appointments: Appointment[];
-
-    constructor() {
-        this.appointments = [];
-    }
-
-    public all(): Appointment[] {
-        return this.appointments;
-    }
-
-    public findByDate(date: Date): Appointment | null {
-        const findAppointmentInSameDate = this.appointments.find(appointment =>
-            isEqual(date, appointment.date),
-        );
-        return findAppointmentInSameDate || null;
-    }
-
-    public create({ provider, date }: CreateAppointmentDTO): Appointment {
-        const appointment = new Appointment({ provider, date });
-        this.appointments.push(appointment);
-        return appointment;
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+    public async findByDate(date: Date): Promise<Appointment | null> {
+        const findAppointment = await this.findOne({
+            where: {
+                date,
+            },
+        });
+        return findAppointment || null;
     }
 }
 
