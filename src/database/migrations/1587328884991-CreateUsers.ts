7f9a0e4eae2849aeaa4eca51a7f28ca5301021f0
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-19 18:36:41 -0300

    Relacionamento nos models

--- src/database/migrations/1587328884991-CreateUsers.ts
+++ src/database/migrations/1587328884991-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587328884991 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
