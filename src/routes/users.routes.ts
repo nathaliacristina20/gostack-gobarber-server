c3d5afafefab634519955142fd8d92aa961a4d07
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-19 19:30:48 -0300

    Criptografia de senha

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -14,6 +14,9 @@ usersRouter.post('/', async (request, response) => {
             email,
             password,
         });
+
+        delete user.password;
+
         return response.json(user);
     } catch (err) {
         return response.status(400).json({ error: err.message });
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -18,10 +19,12 @@ class CreateUserService {
             throw new Error('Email address already used.');
         }
 
+        const hashedPassword = await hash(password, 8);
+
         const user = usersRepository.create({
             name,
             email,
-            password,
+            password: hashedPassword,
         });
 
         await usersRepository.save(user);
