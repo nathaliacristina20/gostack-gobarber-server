2aa3c9c1cf675e1fab2846dda120426d6d1b5936
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-19 23:56:14 -0300

    Servindo arquivos estaticos

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,13 @@ import routes from './routes';
 
 import './database';
 
+import uploadConfig from './config/upload';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
