6b26bef45c4cbd868ca44e684f3021d072bf0412
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-19 23:29:26 -0300

    Validando credenciais

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import AuthenticateService from '../services/AuthenticateService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+    try {
+        const { email, password } = request.body;
+
+        const authenticatorUser = new AuthenticateService();
+
+        const { user } = await authenticatorUser.execute({ email, password });
+
+        delete user.password;
+
+        return response.json({ user });
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default sessionsRouter;
---
+++ src/services/AuthenticateService.ts
@@ -0,0 +1,36 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response {
+    user: User;
+}
+
+class AuthenticateService {
+    public async execute({ email, password }: Request): Promise<Response> {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({
+            where: { email },
+        });
+
+        if (!user) {
+            throw new Error('Incorrect email/password combination.');
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error('Incorrect email/password combination.');
+        }
+
+        return { user };
+    }
+}
+
+export default AuthenticateService;
