033682ca073794f51a46c911185d011551a34151
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-05-06 19:36:48 -0300

    Configurando Jest

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', () => {
+        expect(1 + 2).toBe(3);
+    });
+
+    it('should not be able to create two appointments on the same time', () => {
+        expect(1 + 2).toBe(3);
+    });
+});
