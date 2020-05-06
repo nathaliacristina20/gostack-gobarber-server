18fdf348cdd8f981effc141ba2d5ee243091c8c1
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-05-06 20:35:22 -0300

    Teste de agendamento

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,14 +1,15 @@
 import { uuid } from 'uuidv4';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/iCreateAppointmentDTO';
+import { isEqual } from 'date-fns';
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
     private appointments: Appointment[] = [];
 
     public async findByDate(date: Date): Promise<Appointment | undefined> {
-        const findAppointment = this.appointments.find(
-            appointment => appointment.date === date,
+        const findAppointment = this.appointments.find(appointment =>
+            isEqual(appointment.date, date),
         );
         return findAppointment;
     }
