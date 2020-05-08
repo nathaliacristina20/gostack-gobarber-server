f947d5c2413e61023f63a60b9bc00a2f75c62394
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-05-08 20:22:47 -0300

    Salvando tokens no banco

---
+++ src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+class ForgotPassword {
+    public async create(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const { email } = request.body;
+
+        const sendForgotPasswordEmail = container.resolve(
+            SendForgotPasswordEmailService,
+        );
+
+        await sendForgotPasswordEmail.execute({
+            email,
+        });
+
+        return response.status(204).json();
+    }
+}
+
+export default ForgotPassword;
