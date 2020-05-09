10591f15ae436079ecb58000355e16528471723b
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-05-09 19:36:04 -0300

    Listagem de prestadores

---
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import ListProvidersServices from '@modules/appointments/services/ListProvidersService';
+
+class ProvidersController {
+    public async index(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const user_id = request.user.id;
+
+        const listProviders = container.resolve(ListProvidersServices);
+        const providers = await listProviders.execute({
+            user_id,
+        });
+
+        return response.json(providers);
+    }
+}
+
+export default ProvidersController;
--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -1,8 +1,10 @@
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 import User from '../infra/typeorm/entities/User';
 
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 export default interface IUsersRepository {
+    findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
     findById(id: string): Promise<User | undefined>;
     findByEmail(email: string): Promise<User | undefined>;
     create(data: ICreateUserDTO): Promise<User>;
