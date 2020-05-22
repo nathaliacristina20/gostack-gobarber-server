3fa41743eab8b7e76a72a5a54273b7bbc58198e8
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-05-21 22:02:03 -0300

    Dias indisponiveis no mes

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 // import AppError from '@shared/errors/AppError';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -45,13 +45,16 @@ class ListProviderMonthAvailabilityService {
         );
 
         const avaibility = eachDayArray.map(day => {
+            const compareDate = new Date(year, month - 1, day, 23, 59, 59);
             const appointmentsInDay = appointments.filter(appointment => {
                 return getDate(appointment.date) === day;
             });
 
             return {
                 day,
-                available: appointmentsInDay.length < 10,
+                available:
+                    isAfter(compareDate, new Date()) &&
+                    appointmentsInDay.length < 10,
             };
         });
 
